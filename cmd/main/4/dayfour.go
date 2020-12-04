d263535f1c7e74328d6b9da79259eda0ca65dd51
James Millner <jamesmillnerdev@gmail.com>
2020-12-04 23:09:51 +0000

    Day 4 part 2

--- cmd/main/4/dayfour.go
+++ cmd/main/4/dayfour.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/gobuffalo/packr"
 	"github.com/james-millner/adventofcode2020/pkg/file"
+	"github.com/james-millner/adventofcode2020/pkg/function"
 )
 
-var validFields = []string{
+var requiredFields = []string{
 	"byr",
 	"iyr",
 	"eyr",
@@ -19,6 +21,20 @@ var validFields = []string{
 	"pid",
 }
 
+var optionalFields = []string{
+	"cid",
+}
+
+var hairColours = []string{
+	"amb",
+	"blu",
+	"brn",
+	"gry",
+	"grn",
+	"hzl",
+	"oth",
+}
+
 func main() {
 	box := packr.NewBox("./input")
 	batchPassportFile := file.RetreiveInputFileAsString("dayfour.txt", box)
@@ -26,14 +42,20 @@ func main() {
 	regex := regexp.MustCompile(`(?m)^\s*$`).ReplaceAllString(strings.TrimSpace(batchPassportFile), "-")
 	passports := strings.Split(regex, "-")
 
-	formattedPassports := Map(passports, createPassportString)
+	formattedPassports := function.Map(passports, createPassportString)
 
-	validPassports := filter(formattedPassports, passportVerifier)
+	// for _, v := range formattedPassports {
+	// 	fmt.Printf("%s\n", extendedPassportVerifier(v))
+	// }
 
-	fmt.Printf("%d\n", len(validPassports))
+	validPassportsWithBasicValidation := function.Filter(formattedPassports, basicPassportVerifier)
+	fmt.Printf("Valid passports with basic validation: %d\n", len(validPassportsWithBasicValidation))
+
+	validPassportsWithExtendedValidation := function.Filter(formattedPassports, extendedPassportVerifier)
+	fmt.Printf("Valid passports with extended validation: %d\n", len(validPassportsWithExtendedValidation))
 }
 
-func passportVerifier(passport string) bool {
+func basicPassportVerifier(passport string) bool {
 
 	if passport == "" {
 		return false
@@ -41,38 +63,118 @@ func passportVerifier(passport string) bool {
 
 	numberOfValidFields := 0
 
-	for _, validField := range validFields {
+	for _, validField := range requiredFields {
 		if strings.Contains(passport, validField) {
 			numberOfValidFields++
 		}
 	}
 
-	if numberOfValidFields != len(validFields) {
+	if numberOfValidFields != len(requiredFields) {
 		return false
 	}
 
 	return true
 }
 
-func filter(vs []string, f func(string) bool) []string {
-	vsf := make([]string, 0)
-	for _, v := range vs {
-		if f(v) {
-			vsf = append(vsf, v)
+func extendedPassportVerifier(passport string) bool {
+
+	if !basicPassportVerifier(passport) {
+		return false
+	}
+
+	passportSlice := strings.Split(passport, ",")
+
+	passportMap := make(map[string]string)
+	// put slice values into map
+	for _, piece := range passportSlice {
+		keyValue := strings.Split(piece, ":")
+		passportMap[strings.TrimSpace(keyValue[0])] = strings.TrimSpace(keyValue[1])
+	}
+
+	if val, ok := passportMap["byr"]; ok {
+		byr, _ := strconv.Atoi(val)
+		if byr < 1920 || byr > 2002 {
+			return false
 		}
 	}
-	return vsf
-}
 
-func Map(vs []string, f func(string) string) []string {
-	vsm := make([]string, len(vs))
-	for i, v := range vs {
-		vsm[i] = f(v)
+	if val, ok := passportMap["iyr"]; ok {
+		iyr, _ := strconv.Atoi(val)
+		if iyr < 2010 || iyr > 2020 {
+			return false
+		}
 	}
-	return vsm
+
+	if val, ok := passportMap["eyr"]; ok {
+		expr, _ := regexp.Compile("([0-9]{4})")
+
+		if !expr.MatchString(val) {
+			return false
+		}
+
+		eyr, _ := strconv.Atoi(val)
+		if eyr < 2020 || eyr > 2030 {
+			return false
+		}
+	}
+
+	if val, ok := passportMap["hgt"]; ok {
+		heightAndMetric := regexp.MustCompile("([0-9]+)([a-z]+)").FindStringSubmatch(val)
+
+		if len(heightAndMetric) == 0 {
+			return false
+		}
+
+		heightValue := heightAndMetric[1]
+		heightMetric := heightAndMetric[2]
+
+		if heightMetric == "cm" {
+			val, _ := strconv.Atoi(heightValue)
+			if val < 150 || val > 193 {
+				return false
+			}
+		} else if heightMetric == "in" {
+			val, _ := strconv.Atoi(heightValue)
+			if val < 59 || val > 76 {
+				return false
+			}
+		}
+
+		if heightMetric != "cm" && heightMetric != "in" {
+			return false
+		}
+	}
+
+	if val, ok := passportMap["hcl"]; ok {
+		expr, _ := regexp.Compile("(#[0-9a-f]{6})")
+		if !expr.MatchString(val) {
+			return false
+		}
+	}
+
+	if val, ok := passportMap["ecl"]; ok {
+		if !function.Find(hairColours, val) {
+			return false
+		}
+	}
+
+	if val, ok := passportMap["pid"]; ok {
+		_, err := regexp.MatchString(`(\d{9})`, val)
+		if err != nil || len(val) != 9 {
+			return false
+		}
+
+	}
+
+	return true
 }
 
 func createPassportString(passport string) string {
-	pieces := strings.Split(passport, "\n")
-	return strings.Join(pieces, " ")
+	pieces := strings.FieldsFunc(passport, Split)
+
+	return strings.Join(pieces, ",")
+}
+
+func Split(r rune) bool {
+	return r == '\n' || r == ' '
 }
---
+++ pkg/function/collection_handling.go
@@ -0,0 +1,28 @@
+package function
+
+func Filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
+
+func Map(vs []string, f func(string) string) []string {
+	vsm := make([]string, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+func Find(slice []string, val string) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
